80bad1cb2cf220e305eeb8f0e5ceeff67bf6ad71
Drey0911 <andrey0911a@gmail.com>
2025-11-18 18:24:07 -0500

    API Render

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { AuthResponse, Ticket, User} from '../types';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://api-tickets-lynz.onrender.com/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
